452b01441f7832edc71e5ecc6d2f844cfbf56ba2
2ndAlice <42082204+2ndAlice@users.noreply.github.com>
2024-08-29 08:34:40 +0900

    new
    testtest

---
+++ Dango/dango/src/main/java/com/dango/dango/DangoApplication.java
@@ -0,0 +1,13 @@
+package com.dango.dango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangoApplication.class, args);
+	}
+
+}
